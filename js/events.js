afc13c6ea814c98fbd6d91072f383289db565575
William Chabla <w_chabla@hotmail.com>
2018-05-22 00:49:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
 //define functions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("form").on("keydown", function(key){
+    if(key.which == 71){
+      alert("Key G was pressed.");
+    }
+      return;
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
 
